63465e1f1422ef621226d60c4568902be5a5109a
이우진 <zmfpdl64@naver.com>
2022-02-25 12:45:36 +0900

    API 사용 ResponseBody

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Spring_boot
+
+<h1>자료 참조</h1>
+<h3>김영한의 스프링 입문</h3>
+
+https://inf.run/ufw2
--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -2,6 +2,7 @@ package hello.hellospring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -17,6 +18,44 @@ public class HelloController {
     public String helloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello-mvc";
+    }
+    
+    @GetMapping("hello-string")
+    @ResponseBody   //body부에 리졸버를 사용하지 않고 바로 데이터를 바디에 전달해준다.
+    public String hellostring(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+
+    //api방식
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello(name, 25);
+        return hello;   //Hello 객체를 반환해서 {key : value}를 반환한다.
+    }
+
+     class Hello{   //GETTER SETTER를  CTRL + ENTER  이것을 프로퍼티 접근방식이라 한다.
+        private String name;
+        private int age;
+
+        public Hello(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+        public int getage() {
+            return this.age;
+        }
+        public void setage(int age) {
+            this.age = age;
+        }
     }
     // http://localhost:8080/hello-mvc?name=woojin 이런식으로 인자를 넘길 수 있다.
 }
