9300bbe4a66eb8a9b65ab114826b36f923f483a7
이우진 <zmfpdl64@naver.com>
2022-02-27 08:12:25 +0900

    순수 jdbc를 이용한 db연결

--- hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
@@ -1,20 +1,31 @@
 package hello.hellospring.service;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepoistory;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
 @Configuration
 public class SpringConfig {
 
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    private DataSource dataSource;
+
     @Bean
-    public MemberService memberService() {
+    public MemberService memberService() throws SQLException {
         return new MemberService(memberRepository());
     }
 
     @Bean
-    public MemberRepoistory memberRepository() {
-        return new MemoryMemberRepository();
+    public MemberRepoistory memberRepository() throws SQLException {
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
